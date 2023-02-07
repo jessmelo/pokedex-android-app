21d897a713f994be05f76fc69975502c2d8a9844
jessmelo <jessmelo@usp.br>
2023-02-07 04:00:31 -0300

    Atualizando página de tipos

--- app/src/main/java/com/jessmelo/apps/pokedex/ui/types/PokemonTypesScreen.kt
+++ app/src/main/java/com/jessmelo/apps/pokedex/ui/types/PokemonTypesScreen.kt
@@ -1,14 +1,12 @@
 package com.jessmelo.apps.pokedex.ui.types
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +19,6 @@ import androidx.compose.ui.unit.dp
 import com.jessmelo.apps.pokedex.data.model.pokemonTypes
 import com.jessmelo.apps.pokedex.ui.components.TopBar
 import com.jessmelo.apps.pokedex.ui.theme.PokeLightYellow
-import com.jessmelo.apps.pokedex.ui.theme.PokeVeryLightYellow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,9 +27,7 @@ fun PokemonTypesScreen() {
         Column(
             modifier = Modifier
                 .padding(it)
-                .padding(10.dp)
-                .background(PokeLightYellow)
-                .clip(shape = RoundedCornerShape(20.dp)),
+                .padding(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             pokemonTypes.forEach { (key, value) ->
@@ -46,15 +41,16 @@ fun PokemonTypesScreen() {
 fun PokemonTypeRow(pokemonType: String) {
     Row(
         modifier = Modifier
-            .fillMaxWidth()
             .padding(10.dp)
+            .clip(shape = RoundedCornerShape(20.dp))
+            .background(PokeLightYellow)
+            .fillMaxWidth()
     ) {
         Text(
             text = pokemonType.uppercase(),
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
-                .padding(5.dp)
-                .background(PokeVeryLightYellow)
+                .padding(10.dp)
         )
     }
 }
